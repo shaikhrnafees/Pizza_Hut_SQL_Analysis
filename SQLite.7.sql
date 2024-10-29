99d87cea919f6aa0cd92fb595170093ef3615533
Nafees Shaikh <57799585+shaikhrnafees@users.noreply.github.com>
2024-10-29 17:34:44 +0530

    Delete SQLite.7.sql

--- SQLite.7.sql
+++
@@ -1,5 +0,0 @@
---Determine the distribution of orders by hour of the day.
-select strftime('%H',time) as hour, count(order_id) as distrbution_of_order
-from orders
-group by hour
-order by hour asc
