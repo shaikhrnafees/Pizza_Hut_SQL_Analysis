c1a1f2821fe8323e4e3760ec9090d63a1dcac3b7
Nafees Shaikh <57799585+shaikhrnafees@users.noreply.github.com>
2024-10-29 17:34:27 +0530

    Delete SQLite.4.sql

--- SQLite.4.sql
+++
@@ -1,9 +0,0 @@
---List the top 5 most ordered pizza types along with their quantities.
-
-select p.pizza_type_id ,sum(o.quantity) as Top_5_Most
-from order_details o
-left join pizzas p 
-on o.pizza_id=p.pizza_id
-GROUP by p.pizza_type_id
-order by top_5_most desc
-limit 5;
